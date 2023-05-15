51447f7c64e740d70bae1b762f24cd36cae2b68f
VenoM9078 <roushan.venom1@gmail.com>
2023-05-15 19:57:13 +0500

    Fixes

--- resources/views/dashboard/teacher/viewHistory.blade.php
+++ resources/views/dashboard/teacher/viewHistory.blade.php
@@ -2,10 +2,18 @@
 @section('content')
 <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <!-- BEGIN: Top Bar -->
-<div class="intro-y flex items-center mt-8">
+<div class="intro-y flex items-center mt-8 gap-2">
     <h2 class="text-lg font-medium mr-auto">
         Tool Usage History
     </h2>
+    <div>
+        <span><a target="_blank" class="btn btn-success text-white" href="mailto:ayuda@maestroia.com">Informar de un
+                Error</a></span>
+    </div>
+    <div>
+        <span><a target="_blank" class="btn btn-success text-white" href="mailto:Admin@maestroia.com">Idea para una
+                Herramienta</a></span>
+    </div>
 </div>
 <!-- END: Top Bar -->
 <div class="grid grid-cols-12 gap-6">
@@ -44,14 +52,14 @@
 
 <hr class="border-b mt-5 border-gray-200" />
 
-<div class="mt-5 space-y-2">
+{{-- <div class="mt-5 space-y-2">
     <div class="mb-3">
         <span>Informar de un Error - <a target="_blank" class="btn btn-success text-white"
                 href="mailto:ayuda@maestroia.com">ayuda@maestroia.com</a></span>
     </div>
     <span>Idea para una Herramienta - <a target="_blank" class="btn btn-success text-white"
             href="mailto:Admin@maestroia.com">Admin@maestroia.com</a></span>
-</div>
+</div> --}}
 <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
     crossorigin="anonymous"></script>
 <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
