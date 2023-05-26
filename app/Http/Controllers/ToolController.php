471ef9fdb53cd3019da60f95586756eff6a65b49
VenoM9078 <roushan.venom1@gmail.com>
2023-05-26 15:17:11 +0500

    tokens

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -200,7 +200,7 @@ class ToolController extends Controller
                     ],
                 ],
                 'temperature' => 0.9,
-                'max_tokens' => 700,
+                'max_tokens' => 1000,
                 'frequency_penalty' => 0,
                 'presence_penalty' => 0.6,
             ]);
@@ -468,7 +468,7 @@ class ToolController extends Controller
                     ],
                 ],
                 'temperature' => 0.9,
-                'max_tokens' => 500,
+                'max_tokens' => 1000,
                 'frequency_penalty' => 0,
                 'presence_penalty' => 0,
             ];
