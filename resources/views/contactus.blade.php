36322381d338df3788e5c64f91cf7657c4aa711f
VenoM9078 <roushan.venom1@gmail.com>
2023-06-12 15:37:53 +0500

    canvas fix

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -132,61 +132,11 @@
                                     </button>
                                 </div>
                             </div>
-                            <div class="offcanvas__search mb-25">
-                                <form action="#">
-                                    <input type="text" placeholder="What are you searching for?">
-                                    <button type="submit"><i class="far fa-search"></i></button>
-                                </form>
-                            </div>
+
                             <div class="mobile-menu fix"></div>
-                            <div class="offcanvas__text d-none d-lg-block">
-                                <p>But I must explain to you how all this mistaken idea of denouncing pleasure and
-                                    praising pain was born and will give you a complete account of the system and
-                                    expound the actual teachings of the great explore</p>
-                            </div>
-                            <div class="offcanvas__map d-none d-lg-block mb-15">
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29176.030811137334!2d90.3883827!3d23.924917699999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1605272373598!5m2!1sen!2sbd"></iframe>
-                            </div>
-                            <div class="offcanvas__contact mt-30 mb-20">
-                                <h4>Contact Info</h4>
-                                <ul>
-                                    <li class="d-flex align-items-center">
-                                        <div class="offcanvas__contact-icon mr-15">
-                                            <i class="fal fa-map-marker-alt"></i>
-                                        </div>
-                                        <div class="offcanvas__contact-text">
-                                            <a target="_blank"
-                                                href="https://www.google.com/maps/place/Dhaka/@23.7806207,90.3492859,12z/data=!3m1!4b1!4m5!3m4!1s0x3755b8b087026b81:0x8fa563bbdd5904c2!8m2!3d23.8104753!4d90.4119873">12/A,
-                                                Mirnada City Tower, NYC</a>
-                                        </div>
-                                    </li>
-                                    <li class="d-flex align-items-center">
-                                        <div class="offcanvas__contact-icon mr-15">
-                                            <i class="far fa-phone"></i>
-                                        </div>
-                                        <div class="offcanvas__contact-text">
-                                            <a href="mailto:support@gmail.com">088889797697</a>
-                                        </div>
-                                    </li>
-                                    <li class="d-flex align-items-center">
-                                        <div class="offcanvas__contact-icon mr-15">
-                                            <i class="fal fa-envelope"></i>
-                                        </div>
-                                        <div class="offcanvas__contact-text">
-                                            <a href="tel:+012-345-6789">support@mail.com</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="offcanvas__social">
-                                <ul>
-                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
+
+
+
                         </div>
                     </div>
                 </div>
